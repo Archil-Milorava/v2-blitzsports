154e5178fa76d11fd273699a7935e5964a390432
Archil-Milorava <achimilorava16@gmail.com>
2026-01-09 21:12:26 +0400

    tested models

--- src/features/Articles/actions.ts
+++ src/features/Articles/actions.ts
@@ -1,4 +1,6 @@
+"use server" 
 import { prisma } from '@/lib/db'
+import { revalidatePath } from 'next/cache'
 
 export const getNews = async () => {
   try {
@@ -21,6 +23,36 @@ export const getStories = async () => {
     })
 
     return articles
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message)
+    }
+  }
+}
+
+
+
+export const createArticles = async (formData: FormData) => {
+  try {
+    const title = formData.get('title') as string
+    const content = formData.get('content') as string
+    const category = formData.get('category') as string
+    const badge = formData.get('badge') as 'NEWS' | 'HISTORY'
+    const authorId = formData.get('authorId') as string
+
+    await prisma.article.create({
+      data: {
+        title,
+        content,
+        category,
+        badge,
+        authorId,
+        imageUrl: "https://via.placeholder.com/150", // Placeholder since we're ignoring upload
+      },
+    })
+
+    // This refreshes the page data immediately
+    revalidatePath('/') 
   } catch (error) {
     if (error instanceof Error) {
       console.log(error.message)
