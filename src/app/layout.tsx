e401b98e44c07d517cec5bf2c46116733e27a3a0
Archil-Milorava <achimilorava16@gmail.com>
2026-02-10 21:55:45 +0400

    fresh start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { Toaster } from '@/components/ui/sonner';
-import { TooltipProvider } from '@/components/ui/tooltip';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,8 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TooltipProvider>{children}</TooltipProvider>
-        <Toaster />
+        {children}
       </body>
     </html>
   );
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,2 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
 
-export function proxy(req: NextRequest) {
-  const isLoggedIn = req.cookies.get('better-auth.session_token');
-
-  const { pathname } = req.nextUrl;
-
-  const notAuth = ['/sign-in', '/sign-up'];
-  const mustAuth = ['/profile'];
-
-  if (isLoggedIn && notAuth.some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-
-  if (!isLoggedIn && mustAuth.some((path) => pathname.startsWith(path))) {
-    return NextResponse.redirect(new URL('/sign-in', req.url));
-  }
-}
+export function proxy() {}
